8844140d159f0fa7efa4062671948df5ba2a5ba6
Fabian Pedregosa <fabian@fseoane.net>
2013-05-03 10:50:30 +0200

    cosmetic

--- filters/clean_css.py
+++ filters/clean_css.py
@@ -2,6 +2,11 @@ import sys
 
 data = sys.stdin.read()
 
-# remove clear: both from html
-data = data.replace('clear: both', '')
+# remove "clear: both" from html
+# f = open('tmp.txt', 'r+w')
+# f.write(f.read() + data)
+# f.close()
+if data.find('clear: both;'):
+    data = data.replace('clear: both;', '')
 sys.stdout.write(data)
+
